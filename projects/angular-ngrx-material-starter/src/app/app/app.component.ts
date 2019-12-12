2f3d24c08d5efd68b2f2206ea6859b9e6eb57652
DakoMumladze <dako.mumladze@geolab.edu.de>
2019-12-12 23:30:03 +0400

    Init detailed movie page

--- projects/angular-ngrx-material-starter/src/app/app/app.component.ts
+++ projects/angular-ngrx-material-starter/src/app/app/app.component.ts
@@ -35,8 +35,6 @@ export class AppComponent implements OnInit {
   logo = require('../../assets/logo.png');
   languages = ['en', 'de', 'sk', 'fr', 'es', 'pt-br', 'zh-cn', 'he'];
   navigation = [
-    { link: 'about', label: 'anms.menu.about' },
-    { link: 'feature-list', label: 'anms.menu.features' },
     { link: 'examples', label: 'anms.menu.examples' }
   ];
   navigationSideMenu = [
